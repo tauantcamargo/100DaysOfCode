f95c817cd187f7b77aaf5aa6fc9a069c69b560c7
Tauan Camargo <lukyhenson@gmail.com>
2024-06-02 19:06:21 -0300

    feat: create base program using previous knowledge

---
+++ day1/main.py
@@ -0,0 +1,12 @@
+def main():
+    print("Welcome to the Band Name Generator.")
+    print("What's the name opf the city you grew up in?")
+    city = input("")
+    print("Whtat's your pet's name?")
+    pet = input("")
+
+    bandname = city + " " + pet
+    print(bandname)
+
+
+main()
